b561d129c361997504d9b6b23075ac1be16551f9
Ju Liu <ju.liu@welaika.com>
2016-09-28 17:51:02 +0100

    Implement new supervision tree structure

--- pooly/lib/pooly/worker_supervisor.ex
+++ pooly/lib/pooly/worker_supervisor.ex
@@ -3,20 +3,22 @@ defmodule Pooly.WorkerSupervisor do
 
   # API
 
-  def start_link({_,_,_} = mfa) do
-    Supervisor.start_link(__MODULE__, mfa)
+  def start_link(pool_server, {_,_,_} = mfa) do
+    Supervisor.start_link(__MODULE__, [pool_server, mfa])
   end
 
   # Callbacks
 
-  def init({m,f,a}) do
+  def init([pool_server, {m,f,a}]) do
+    Process.link(pool_server)
+
     worker_opts = [
-      restart: :permanent,
+      restart: :temporary,
+      shutdown: 5000,
       function: f
     ]
 
     children = [worker(m, a, worker_opts)]
-
     opts = [
       strategy: :simple_one_for_one,
       max_restarts: 5,
