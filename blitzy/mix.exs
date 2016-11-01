cb145541f8c56ff7b296e0e2b4d77821bedff38a
Ju Liu <ju.liu@welaika.com>
2016-11-01 20:26:17 +0000

    Finish up blitzy

--- blitzy/mix.exs
+++ blitzy/mix.exs
@@ -5,6 +5,7 @@ defmodule Blitzy.Mixfile do
     [app: :blitzy,
      version: "0.1.0",
      elixir: "~> 1.3",
+     escript: [main_module: Blitzy.CLI],
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
