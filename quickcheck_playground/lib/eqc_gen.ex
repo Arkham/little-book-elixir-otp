9cd39eebd52cb5fa3e5382ebc1d22e3814b3e1d4
Ju Liu <ju.liu@welaika.com>
2016-11-09 15:46:45 +0000

    Some recursive generators

--- quickcheck_playground/lib/eqc_gen.ex
+++ quickcheck_playground/lib/eqc_gen.ex
@@ -17,4 +17,38 @@ defmodule EQCGen do
          {1, ?,}]))
     end
   end
+
+  def nested_list(gen) do
+    sized size do
+      nested_list(size, gen)
+    end
+  end
+
+  defp nested_list(0, _gen), do: []
+  defp nested_list(n, gen) do
+    lazy do
+      oneof [
+        [gen|nested_list(n-1, gen)],
+        [nested_list(n-1, gen)]
+      ]
+    end
+  end
+
+  def balanced_tree(gen) do
+    sized size do
+      balanced_tree(size, gen)
+    end
+  end
+
+  def balanced_tree(0, gen), do: {:leaf, gen}
+
+  def balanced_tree(n, gen) do
+    lazy do
+      {:node,
+       gen,
+       balanced_tree(div(n, 2), gen),
+       balanced_tree(div(n, 2), gen)}
+    end
+  end
+
 end
